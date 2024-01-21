c25c432d602a4b9508618bdd42d0b734f835847b
“domekenshi” <kfprog5136@gmail.com>
2024-01-22 00:04:38 +0900

    animation practioce

--- TestApps/App.tsx
+++ TestApps/App.tsx
@@ -9,6 +9,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {RootStackParamList} from './src/types/RootStackParamList';
 import HomeScreen from './src/pages/HomeScreen';
+import Todo from './src/pages/Todo';
+import Animation from './src/pages/Animation';
+
 import Detail from './src/pages/detail';
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -17,7 +20,9 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Todo" component={Todo} />
         <Stack.Screen name="Detail" component={Detail} />
+        <Stack.Screen name="Animation" component={Animation} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- TestApps/src/types/RootStackParamList.tsx
+++ TestApps/src/types/RootStackParamList.tsx
@@ -1,4 +1,6 @@
 export type RootStackParamList = {
   Home: undefined; // ホーム画面にはパラメータが不要
+  Todo: {itemId: number; otherParam: string}; // 詳細画面にはitemIdとotherParamが必要
   Detail: {itemId: number; otherParam: string}; // 詳細画面にはitemIdとotherParamが必要
+  Animation: {itemId: number; otherParam: string}; // 詳細画面にはitemIdとotherParamが必要
 };
